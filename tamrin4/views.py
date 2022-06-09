d746d2189a3b2936c0dc6f5cf99c3d6319a34fac
Mehdi Alimoradi <alimoradi.viramadar@gmail.com>
2022-06-09 19:25:38 +0430

    broooo

--- tamrin4/views.py
+++ tamrin4/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
-# def index_page(request):
-#     return render(request,'index.html')
 def index_page(request):
-    return HttpResponse('hellowwwwwwwwwww')
+    return render(request,'index.html')
+# def index_page(request):
+#     return HttpResponse('hellowwwwwwwwwww')
