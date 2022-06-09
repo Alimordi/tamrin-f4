840539ef0273b5eab8ab6b92c83237264a9c0f75
Mehdi Alimoradi <alimoradi.viramadar@gmail.com>
2022-06-09 18:52:06 +0430

    helooowwwww

--- tamrin4/views.py
+++ tamrin4/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
+# def index_page(request):
+#     return render(request,'index.html')
 def index_page(request):
-    return render(request,'index.html')
+    return HttpResponse('hellowwwwwwwwwww')
